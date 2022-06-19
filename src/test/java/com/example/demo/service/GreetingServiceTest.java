4f786bb68cbc25cbaa92abf3dd991d4a579bb818
ainvool <ain.voolma@gmail.com>
2022-06-19 13:02:01 +0300

    [add] - simple injection test created

---
+++ src/test/java/com/example/demo/service/GreetingServiceTest.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest // prepares all required beans for testing purposes
+public class GreetingServiceTest {
+
+    @Autowired
+    private  GreetingService greetingService;
+
+    @Test
+    public void injectionTest() {
+        Assertions.assertNotNull(greetingService, "Greeting service shouldn't be null!!!");
+    }
+}
