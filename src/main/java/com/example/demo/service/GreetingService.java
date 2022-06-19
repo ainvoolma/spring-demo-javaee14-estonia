c3702273a4176031b81b7fa5813a74c0db054b55
ainvool <ain.voolma@gmail.com>
2022-06-19 11:37:26 +0300

    [add] - creating GreetingService

---
+++ src/main/java/com/example/demo/service/GreetingService.java
@@ -0,0 +1,17 @@
+package com.example.demo.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+
+public class GreetingService {
+
+    public String makeSomeGreetingToUser(String name) {
+        String result = "Welcome to Spring class" + name;
+
+        log.info(result);
+        return result;
+    }
+}
